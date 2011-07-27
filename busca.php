8ae644d253073018d430f39f5e0a7ae4833d88d2
alanmartini <alanmmartini@gmail.com>
2011-07-26 21:21:21 -0300

    comecando listagem

--- busca.php
+++ busca.php
@@ -3,25 +3,62 @@ require_once '_config.php';
 
 ConectarBanco();
 
+$sql = '';
+
 if (post('buscar')) {
     switch (post('filtro')) {
         case 'vigencia':
-            $sql = mysql_query('SELECT * FROM AUTO WHERE VIGENCIA_FIM BETWEEN  "'.formatarDataEN(post('buscar')).'" AND "'.formatarDataEN(post('buscar_fim')).'" UNION ALL
-                                SELECT * FROM RE WHERE VIGENCIA_FIM BETWEEN "'.formatarDataEN(post('buscar')).'" AND "'.formatarDataEN(post('buscar_fim')).'" ');
+            $sqlAuto = mysql_query('SELECT * FROM AUTO WHERE VIGENCIA_FIM BETWEEN  "'.formatarDataBR(post('buscar')).'" AND "'.formatarDataBR(post('buscar_fim')).'"');
+            $sqlRe   = mysql_query('SELECT * FROM RE WHERE VIGENCIA_FIM BETWEEN "'.formatarDataBR(post('buscar')).'" AND "'.formatarDataBR(post('buscar_fim')).'"');
             break;
         case 'placa':
-            $sql = mysql_query('SELECT * FROM AUTO WHERE PLACA = "'.post('buscar').'"');
+            $sql = mysql_query('SELECT * FROM AUTO WHERE PLACA LIKE "%'.post('buscar').'%"');
             break;
         case 'descricao':
-            $sql = mysql_query('SELECT * FROM AUTO WHERE DESCRICAO = "'.post('buscar').'"');
+            $sql = mysql_query('SELECT * FROM AUTO WHERE DESCRICAO LIKE "%'.post('buscar').'%"');
             break;
         case 'endereco':
-            $sql = mysql_query('SELECT * FROM AUTO WHERE ENDERECO = "'.post('buscar').'"');
+            $sqlRe = mysql_query('SELECT * FROM RE WHERE ENDERECO LIKE "%'.post('buscar').'%"');
             break;        
     }
 } else {
-     $sql = mysql_query('SELECT * FROM AUTO WHERE VIGENCIA_FIM BETWEEN  "'.date("Y-m-1").'" AND "'.date("Y-m-t").'" UNION ALL
-                         SELECT * FROM RE WHERE VIGENCIA_FIM BETWEEN "'.date("Y-m-1").'" AND "'.date("Y-m-t").'" ');
+    $sqlAuto = mysql_query('SELECT * FROM AUTO WHERE VIGENCIA_FIM BETWEEN  "'.date("Y-m-1").'" AND "'.date("Y-m-t").'"');
+    $sqlRe   = mysql_query('SELECT * FROM RE WHERE VIGENCIA_FIM BETWEEN "'.date("Y-m-1").'" AND "'.date("Y-m-t").'"');
+}
+$dadosLista = array();
+if ($sql) {
+    while ($linha = mysql_fetch_array($sql)):
+        array_push($dadosLista, array("0" => @$linha['ID'],
+                                      "1" => @$linha['TIPO_CADASTRO'],
+                                      "2" => @$linha['CLIENTE_ID'],
+                                      "3" => formatarDataEN(@$linha['VIGENCIA_FIM']),
+                                      "4" => @$linha['DESCRICAO'],
+                                      "5" => @$linha['PLACA'],
+                                      "6" => @$linha['CIA_ID']));
+    endwhile;
+} else {
+    //echo 'SELECT ID FROM AUTO WHERE VIGENCIA_FIM BETWEEN  "'.date("Y-m-1").'" AND "'.date("Y-m-t").'"'.'SELECT ID FROM RE WHERE VIGENCIA_FIM BETWEEN "'.date("Y-m-1").'" AND "'.date("Y-m-t").'"';
+    $dadosAuto = array();
+    $dadosRE = array();
+    while ($linhaAuto = @mysql_fetch_array($sqlAuto)):
+        array_push($dadosAuto, array("0" => @$linhaAuto['ID'],
+                                     "1" => @$linhaAuto['TIPO_CADASTRO'],
+                                     "2" => @$linhaAuto['CLIENTE_ID'],
+                                     "3" => formatarDataEN(@$linhaAuto['VIGENCIA_FIM']),
+                                     "4" => @$linhaAuto['DESCRICAO'],
+                                     "5" => @$linhaAuto['PLACA'],
+                                     "6" => @$linhaAuto['CIA_ID'] ));
+    endwhile;
+    while ($linhaRE = mysql_fetch_array($sqlRe)):
+        array_push($dadosRE, array("0" => @$linhaRE['ID'],
+                                   "1" => @$linhaRE['TIPO_CADASTRO'],
+                                   "2" => @$linhaRE['CLIENTE_ID'],
+                                   "3" => formatarDataEN(@$linhaRE['VIGENCIA_FIM']),
+                                   "4" => @$linhaRE['ENDERECO'],
+                                   "5" => @$linhaRE['OCUPACAO'],
+                                   "6" => @$linhaRE['CIA_ID'] ));
+    endwhile;
+    $dadosLista = array_merge($dadosAuto, $dadosRE);
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -53,7 +90,7 @@ if (post('buscar')) {
                     <fieldset>
                     <legend>Filtros de Pesquisa</legend>
                     <div class="pesquisa">
-                        <input class="buscar" style="width:120px !important;" type="text" name="buscar" value="<?php if (post('buscar')){ echo post('buscar');} ?>" />
+                        <input class="buscar" style="width:120px !important;" type="text" name="buscar" id="busca" value="<?php if (post('buscar')){ echo post('buscar');} ?>" />
                         <input class="buscar" style="width:120px !important;" type="text" name="buscar_fim" id="busca_fim" value="<?php if (post('buscar_fim')){ echo post('buscar_fim');} ?>" />
                         <select id="filtro" style="width:120px !important;" name="filtro" class="buscar">
                           <option value="vigencia"  <?php echo selected(post('filtro'), 'vigencia'); ?> >Vigência</option>
@@ -69,7 +106,7 @@ if (post('buscar')) {
                     <table class="tablesorter">
                         <thead>
                             <tr>
-                                <th><!--.icone com tipo de documento inserido.--></th>
+                                <th>!<!--.icone com tipo de documento inserido.--></th>
                                 <th>Cliente</th>
                                 <th>Vigencia</th>
                                 <th>Veículo/Endereço</th>
@@ -79,40 +116,43 @@ if (post('buscar')) {
                             </tr>
                         </thead>
                         <tbody>
-
-                            <!--           rotina para criar array com documentos auto                -->
                         <?php
-                        while ($autoArray = mysql_fetch_array($sql)) :
+                        foreach($dadosLista as $listagem) :
                         ?>
                             <tr class="item autoLinha">
                                 <td><?php
-                            if ($autoArray['TIPO_CADASTRO'] == 'A') {
-                                echo '<img src="img/icons/case_icon&16.png" title="Apolice" />';
-                            };
-                            if ($autoArray['TIPO_CADASTRO'] == 'P') {
-                                echo '<a href="documentos.php?acao=novaApoliceAuto&id=' . $autoArray['ID'] . '" id="cliente-' . $autoArray['ID'] . '"> <img src="img/icons/doc_lines_icon&16.png" title="Atualizar Proposta" /></a>';
-                            };
-                            if ($autoArray['TIPO_CADASTRO'] == 'C') {
-                                echo '<img src="img/icons/calc_icon&16.png" title="->Calculo" />';
-                            };
+                                switch ($listagem[1]) {
+                                    case 'A':
+                                        echo '<img src="img/icons/case_icon&16.png" title="Apolice" />';
+                                        break;
+                                    case 'P':
+                                        echo '<a href="documentos.php?acao=novaApoliceAuto&id=' . $listagem[0] . '" id="cliente-' . $listagem[0] . '">
+                                              <img src="img/icons/doc_lines_icon&16.png" title="Atualizar Proposta" /></a>';
+                                        break;
+                                    case 'C':
+                                        echo '<img src="img/icons/calc_icon&16.png" title="->Calculo" />';
+                                        break;
+                                }
+                                $cliente = mysql_fetch_array(mysql_query('SELECT NOME FROM CLIENTES WHERE ID = ' .$listagem[2] ));
+                                $cia = mysql_fetch_array(mysql_query('SELECT DESCRICAO FROM CIA WHERE ID = ' .$listagem[6] ));
                         ?>
                             </td>
-                            <td>Cliente </td>
-                            <td><?php //echo formatarDataEN($autoArray['VIGENCIA_FIM']);     ?></td>
-                            <td><?php echo $autoArray['DESCRICAO']; ?></td>
-                            <td><?php echo $autoArray['PLACA']; ?></td>
-                            <td><?php //echo Cia::nomeCia($autoArray['CIA_ID'])     ?></td>
+                            <td><?php echo $cliente['NOME']; ?> </td>
+                            <td><?php echo $listagem[3]; ?></td>
+                            <td><?php echo $listagem[4]; ?></td>
+                            <td><?php echo $listagem[5]; ?></td>
+                            <td><?php echo $cia['DESCRICAO']; ?></td>
                             <td style="text-align: right; padding-right: 10px">
-                                <? if ($autoArray['TIPO_CADASTRO'] == 'A') {
+                                <? if ($listagem[1] == 'A') {
                                 ?>
-                                <img onclick="javascript:window.location='documentos.php?acao=renovar&tipoRenovar=auto&id=<?=$autoArray['ID'] ?>'" style="cursor: pointer;" src="img/icons/doc_plus_icon&16.png" title="Renovar" />
-                                <img onclick="javascript:window.location='documentos.php?acao=endossar&tipoEndossar=auto&id=<?=$autoArray['ID'] ?>'" style="cursor: pointer;" src="img/icons/doc_new_icon&16.png" title="Endosso" />
+                                <img onclick="javascript:window.location='documentos.php?acao=renovar&tipoRenovar=auto&id=<?=$listagem[0] ?>'" style="cursor: pointer;" src="img/icons/doc_plus_icon&16.png" title="Renovar" />
+                                <img onclick="javascript:window.location='documentos.php?acao=endossar&tipoEndossar=auto&id=<?=$listagem[0] ?>'" style="cursor: pointer;" src="img/icons/doc_new_icon&16.png" title="Endosso" />
                                 <? }; ?>
-                                <img alt="Editar" onclick="javascript:window.location='documentos.php?acao=editar&tipoEditar=auto&id=<?=$autoArray['ID'] ?>'" style="cursor: pointer;" src="img/icons/doc_edit_icon&16.png" />
+                                <img alt="Editar" onclick="javascript:window.location='documentos.php?acao=editar&tipoEditar=auto&id=<?=$listagem[0] ?>'" style="cursor: pointer;" src="img/icons/doc_edit_icon&16.png" />
                             </td>
                         </tr>
                         <?php
-                                endwhile;
+                            endforeach;
                         ?>
                             </tbody>
                         </table>
@@ -124,8 +164,12 @@ if (post('buscar')) {
         <script type="text/javascript">
             $("#filtro").change(function () {
                 if($(this).val() == 'vigencia'){
+                    $('#busca_fim').val("");
+                    $('#busca').val("");
                     $('#busca_fim').show(450);
                 } else {
+                    $('#busca_fim').val("");
+                    $('#busca').val("");
                     $('#busca_fim').hide(450);
                 }
             })
